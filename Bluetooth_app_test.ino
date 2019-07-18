7129d31f2ffbf630dca0f9d89f6c8ba029145520
Akash Jaiswar <akash.jaiswar41@gmail.com>
2019-07-18 18:18:30 +0530

    Update Bluetooth_app_test.ino
    Can recieve different string it isnt some proof method its a workaround

--- Bluetooth_app_test.ino
+++ Bluetooth_app_test.ino
@@ -1,5 +1,7 @@
 String message;
+String message1;
   int count = 0;
+  int count1 = 0;
   int received = 180;
   
 void setup() {
@@ -14,13 +16,26 @@ void loop() {
   // put your main code here, to run repeatedly:
    while(Serial.available()) {
      //received = Serial.read();
+    if(count <= 3 && count1 == 0){
     message+=char(Serial.read());
-    count++;
+    count++;count1 = 0;
     if(count == 3){
+      Serial.print("First : ");
       Serial.println(message);
       count = 0;
       message = "";  
-    }    
+    }}
+    if(count1 <= 3 && count == 0){
+    message1+=char(Serial.read());
+    count1++;count = 0;
+    if(count1 == 3){
+      Serial.print("Second : ");
+      Serial.println(message1);
+      count1 = 0;
+      message1 = "";  
+      //Serial.print("OK");
+    }
+    }
     }
     digitalWrite(13, HIGH);
     delay(1000);
